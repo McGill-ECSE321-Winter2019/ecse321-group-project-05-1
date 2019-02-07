ab6b0df854150816dd08c03bdcc8c2b87c410dc8
archit-gupta <architgupta3132@gmail.com>
2019-02-06 19:18:14 -0500

    Initital commit of the backend application. New Cooperator folder created

---
+++ Cooperator/src/main/java/application/CoopApplication.java
@@ -0,0 +1,11 @@
+package application;
+
+public class CoopApplication {
+
+	public static void main(String[] args) {
+		// start UI
+        System.out.println("This is group 5. Currently we have nothing.");
+        
+	}
+
+}
---
+++ Cooperator/src/test/java/Cooperator/CoopTest.java
@@ -0,0 +1,11 @@
+package java;
+
+public class CoopTest {
+
+	public static void main(String[] args) {
+		// start UI
+        System.out.println("This is group 5. This is the testing file. Probably where the persistence will be tested.");
+        
+	}
+
+}
